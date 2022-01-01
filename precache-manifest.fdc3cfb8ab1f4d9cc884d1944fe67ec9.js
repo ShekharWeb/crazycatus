6d82b528806127818074020fd52771b4ad93dd1a
unknown <technicalbesmile@gmail.com>
2022-01-01 11:31:47 +0530

    Updates

---
+++ precache-manifest.fdc3cfb8ab1f4d9cc884d1944fe67ec9.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "1c5158cc182e34e658e5",
+    "url": "./static/js/main.1c5158cc.chunk.js"
+  },
+  {
+    "revision": "f3ba7de12a34507d4d61",
+    "url": "./static/js/1.f3ba7de1.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "4e07fd349c460494c0063e2e72f7ea8c",
+    "url": "./index.html"
+  }
+];
